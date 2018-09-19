c1199166ce3497165f878f31c2c82dfaff36d909
Nghia Le <s3654028@rmit.edu.vn>
2018-09-19 10:28:19 +0700

    Display current weather, weather type, city according to location

--- Extras/Extras.swift
+++ Extras/Extras.swift
@@ -7,3 +7,7 @@
 //
 
 import Foundation
+
+let FORECAST_API_URL = "https://api.openweathermap.org/data/2.5/weather?lat=10.83&lon=106.67&appid=fa85c2b8fdbf4cdffc23acc60e9205e9"
+
+typealias DownloadComplete = () -> ()
